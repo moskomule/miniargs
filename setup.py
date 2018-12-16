db07a63509b5822a6765a42272a41651dfb17f0a
moskomule <hataya@keio.jp>
2018-12-17 06:42:01 +0900

    fix setup

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ setup(name="miniargs",
       version="0.1",
       description="a simple argument parser",
       author="Ryuichiro Hataya",
-      author_email="",
+      author_email="hataya@nlab.jp",
       python_requires=">=3.6.0",
       url="https://github.com/moskomule/miniargs",
       py_modules=["miniargs"],
       license="MIT",
       classifiers=["License :: OSI Approved :: MIT License",
-                   "Programming Language :: Python :: 3.6"]
+                   "Programming Language :: Python :: 3.6",
+                   "Programming Language :: Python :: 3.7"]
       )
