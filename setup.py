a49b597488aeb1236a7af85bad13ef5836ca9be7
moskomule <hataya@keio.jp>
2018-12-19 12:04:07 +0900

    fix setup

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
+from pathlib import Path
+
 import setuptools
 
+with (Path(__file__).parent / "README.md").open() as f:
+    long_description = f.read()
+
 setuptools.setup(name="miniargs",
-                 version="0.1.0",
+                 version="0.2.0",
                  description="a simple argument parser",
+                 long_description=long_description,
+                 long_description_content_type='text/markdown',
                  author="Ryuichiro Hataya",
                  author_email="hataya@nlab.jp",
                  python_requires=">=3.6.0",
                  url="https://github.com/moskomule/miniargs",
-                 packages=setuptools.find_packages(),
+                 packages=setuptools.find_packages(exclude=["test"]),
                  license="MIT",
                  classifiers=["License :: OSI Approved :: MIT License",
                               "Programming Language :: Python :: 3.6",
